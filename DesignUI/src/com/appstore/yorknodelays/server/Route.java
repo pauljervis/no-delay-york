a087747b9c30e63bbfc030ce6d7629563a2b1ded
pauljervis <treejervis@nas18-76.york.ac.uk>
2013-04-21 02:42:47 +0100

    Updated getters and setters updated

--- DesignUI/src/com/appstore/yorknodelays/server/Route.java
+++ DesignUI/src/com/appstore/yorknodelays/server/Route.java
@@ -6,7 +6,7 @@ public class Route {
 	private String id;
 	private Airline airline;
 	private float journeyDistance;  // Journey distance in miles
-	private float journeyTime;
+	private int journeyTime;
 	private Airport sourceAirport;
 	private Gate sourceGate;
 	private Airport destinationAirport;
@@ -14,5 +14,57 @@ public class Route {
 	private Weather weather;
 	private Airzone airzoneIntersected;
 	private boolean active;
+	
+	public void setName(String name)					{ this.name = name; }
+	public void setId(String id) 						{ this.id = id; }
+	public void setAirline(Airline airline)				{ this.airline = airline; }
+	public void setJourneyDistance(float distance)		{ this.journeyDistance = distance; }
+	public void setJourneyTime(int journeyTime)			{ this.journeyTime = journeyTime; }
+	public void setSourceAirport(Airport airport)		{ this.sourceAirport = airport; }
+	public void setSourceGate(Gate gate)				{ this.sourceGate = gate; }
+	public void setDestinationAirport(Airport airport)	{ this.destinationAirport = airport; }
+	public void setDestinationGate(Gate gate)			{ this.destinationGate = gate; }
+	public void setWeather(Weather weather)				{ this.weather = weather; }
+	public void setAirzoneIntersected(Airzone airzone)	{ this.airzoneIntersected = airzone; }
+	public void setActive(boolean active)				{ this.active = active; }
+	
+	public String getName() {
+		return name;
+	}
+	public String getId() {
+		return id;
+	}
+	public Airline getAirline() {
+		return airline;
+	}
+	public float getJourneyDistance() {
+		return journeyDistance;
+	}
+	public int getJourneyTime() {
+		return journeyTime;
+	}
+	public Airport getSourceAirport() {
+		return sourceAirport;
+	}
+	public Gate getSourceGate() {
+		return sourceGate;
+	}
+	public Airport getDestinationAirport() {
+		return destinationAirport;
+	}
+	public Gate getDestinationGate() {
+		return destinationGate;
+	}
+	public Weather getWeather() {
+		return weather;
+	}
+	public Airzone getAirzoneIntersected() {
+		return airzoneIntersected;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	
+	
 	
 }
--- DesignUI/src/com/appstore/yorknodelays/server/Schedule.java
+++ DesignUI/src/com/appstore/yorknodelays/server/Schedule.java
@@ -10,5 +10,42 @@ public class Schedule {
 	private List<Airport> destinationAirports;
 	private List<Aircraft> aircraft;
 	private List<Boolean> active;
+	
+	public List<Flight> getFlights() {
+		return flights;
+	}
+	public void setFlights(List<Flight> flights) {
+		this.flights = flights;
+	}
+	public List<Airline> getAirlines() {
+		return airlines;
+	}
+	public void setAirlines(List<Airline> airlines) {
+		this.airlines = airlines;
+	}
+	public List<Airport> getSourceAirports() {
+		return sourceAirports;
+	}
+	public void setSourceAirports(List<Airport> sourceAirports) {
+		this.sourceAirports = sourceAirports;
+	}
+	public List<Airport> getDestinationAirports() {
+		return destinationAirports;
+	}
+	public void setDestinationAirports(List<Airport> destinationAirports) {
+		this.destinationAirports = destinationAirports;
+	}
+	public List<Aircraft> getAircraft() {
+		return aircraft;
+	}
+	public void setAircraft(List<Aircraft> aircraft) {
+		this.aircraft = aircraft;
+	}
+	public List<Boolean> getActive() {
+		return active;
+	}
+	public void setActive(List<Boolean> active) {
+		this.active = active;
+	}
 
 }
