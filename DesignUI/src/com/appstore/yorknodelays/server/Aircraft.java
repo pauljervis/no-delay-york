edf689e2a705d860277138c09c542b7be34ec11c
bzbiohazard@gmail.com <treejervis@nas18-76.york.ac.uk>
2013-04-21 03:39:55 +0100

    Refilled class attributes 2

--- DesignUI/src/com/appstore/yorknodelays/server/Aircraft.java
+++ DesignUI/src/com/appstore/yorknodelays/server/Aircraft.java
@@ -31,6 +31,193 @@ public class Aircraft {
 	private Date commissionDate;
 	private Date nextService;
 	private int prepTime;			// Time to prepare the plane to fly again, seconds
+	private int flightTime;
+	private float bearing;
+	private int departureTime;
+	
+	
+	public float getFuelCapacity() {
+		return fuelCapacity;
+	}
+	public int getRefuleTime() {
+		return refuleTime;
+	}
+	public int getAverageSpeedClimb() {
+		return averageSpeedClimb;
+	}
+	public int getAverageSpeedCruise() {
+		return averageSpeedCruise;
+	}
+	public int getAverageSpeedDescent() {
+		return averageSpeedDescent;
+	}
+	public int getMaxSpeedClimb() {
+		return maxSpeedClimb;
+	}
+	public int getMaxSpeedCruise() {
+		return maxSpeedCruise;
+	}
+	public int getMaxSpeedDescent() {
+		return maxSpeedDescent;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public String getType() {
+		return type;
+	}
+	public String getModel() {
+		return model;
+	}
+	public int getEngines() {
+		return engines;
+	}
+	public float getHourlyRunningCost() {
+		return hourlyRunningCost;
+	}
+	public int getStaffCapacity() {
+		return staffCapacity;
+	}
+	public int getPassangerCapacity() {
+		return passangerCapacity;
+	}
+	public float getAverageTakeoffCost() {
+		return averageTakeoffCost;
+	}
+	public float getRange() {
+		return range;
+	}
+	public float getLenght() {
+		return lenght;
+	}
+	public int getCargoCapacity() {
+		return cargoCapacity;
+	}
+	public int getMaxAltitude() {
+		return maxAltitude;
+	}
+	public Pilot getPilot() {
+		return pilot;
+	}
+	public Controller getController() {
+		return controller;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public Date getCommissionDate() {
+		return commissionDate;
+	}
+	public Date getNextService() {
+		return nextService;
+	}
+	public int getPrepTime() {
+		return prepTime;
+	}
+	public void setFuelCapacity(float fuelCapacity) {
+		this.fuelCapacity = fuelCapacity;
+	}
+	public void setRefuleTime(int refuleTime) {
+		this.refuleTime = refuleTime;
+	}
+	public void setAverageSpeedClimb(int averageSpeedClimb) {
+		this.averageSpeedClimb = averageSpeedClimb;
+	}
+	public void setAverageSpeedCruise(int averageSpeedCruise) {
+		this.averageSpeedCruise = averageSpeedCruise;
+	}
+	public void setAverageSpeedDescent(int averageSpeedDescent) {
+		this.averageSpeedDescent = averageSpeedDescent;
+	}
+	public void setMaxSpeedClimb(int maxSpeedClimb) {
+		this.maxSpeedClimb = maxSpeedClimb;
+	}
+	public void setMaxSpeedCruise(int maxSpeedCruise) {
+		this.maxSpeedCruise = maxSpeedCruise;
+	}
+	public void setMaxSpeedDescent(int maxSpeedDescent) {
+		this.maxSpeedDescent = maxSpeedDescent;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public void setEngines(int engines) {
+		this.engines = engines;
+	}
+	public void setHourlyRunningCost(float hourlyRunningCost) {
+		this.hourlyRunningCost = hourlyRunningCost;
+	}
+	public void setStaffCapacity(int staffCapacity) {
+		this.staffCapacity = staffCapacity;
+	}
+	public void setPassangerCapacity(int passangerCapacity) {
+		this.passangerCapacity = passangerCapacity;
+	}
+	public void setAverageTakeoffCost(float averageTakeoffCost) {
+		this.averageTakeoffCost = averageTakeoffCost;
+	}
+	public void setRange(float range) {
+		this.range = range;
+	}
+	public void setLenght(float lenght) {
+		this.lenght = lenght;
+	}
+	public void setCargoCapacity(int cargoCapacity) {
+		this.cargoCapacity = cargoCapacity;
+	}
+	public void setMaxAltitude(int maxAltitude) {
+		this.maxAltitude = maxAltitude;
+	}
+	public void setPilot(Pilot pilot) {
+		this.pilot = pilot;
+	}
+	public void setController(Controller controller) {
+		this.controller = controller;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public void setCommissionDate(Date commissionDate) {
+		this.commissionDate = commissionDate;
+	}
+	public void setNextService(Date nextService) {
+		this.nextService = nextService;
+	}
+	public void setPrepTime(int prepTime) {
+		this.prepTime = prepTime;
+	}
+	public int getFlightTime() {
+		return flightTime;
+	}
+	public float getBearing() {
+		return bearing;
+	}
+	public int getDepartureTime() {
+		return departureTime;
+	}
+	public void setFlightTime(int flightTime) {
+		this.flightTime = flightTime;
+	}
+	public void setBearing(float bearing) {
+		this.bearing = bearing;
+	}
+	public void setDepartureTime(int departureTime) {
+		this.departureTime = departureTime;
+	}
+	
+	
 	
 
 	
--- DesignUI/src/com/appstore/yorknodelays/server/Runway.java
+++ DesignUI/src/com/appstore/yorknodelays/server/Runway.java
@@ -13,5 +13,60 @@ public class Runway {
 	private boolean held;
 	private int cleanTime;
 	private List<Airline> airline;
+
+	public Airport getAirport() {
+		return airport;
+	}
+	public void setAirport(Airport airport) {
+		this.airport = airport;
+	}
+	public float getBearing1() {
+		return bearing1;
+	}
+	public void setBearing1(float bearing1) {
+		this.bearing1 = bearing1;
+	}
+	public float getBearing2() {
+		return bearing2;
+	}
+	public void setBearing2(float bearing2) {
+		this.bearing2 = bearing2;
+	}
+	public Weather getWeather() {
+		return weather;
+	}
+	public void setWeather(Weather weather) {
+		this.weather = weather;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	public boolean isInUse() {
+		return inUse;
+	}
+	public void setInUse(boolean inUse) {
+		this.inUse = inUse;
+	}
+	public boolean isHeld() {
+		return held;
+	}
+	public void setHeld(boolean held) {
+		this.held = held;
+	}
+	public int getCleanTime() {
+		return cleanTime;
+	}
+	public void setCleanTime(int cleanTime) {
+		this.cleanTime = cleanTime;
+	}
+	public List<Airline> getAirline() {
+		return airline;
+	}
+	public void setAirline(List<Airline> airline) {
+		this.airline = airline;
+	}
 	
 }
