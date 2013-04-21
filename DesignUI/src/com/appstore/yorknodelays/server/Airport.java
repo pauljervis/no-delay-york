17d8dd09812ab57457a9b84f5aee4dfaa01aac94
Horacio Hoyos <hhr502@york.ac.uk>
2013-04-21 01:23:02 +0100

    - Work on the base classes

--- DesignUI/src/com/appstore/yorknodelays/server/Airport.java
+++ DesignUI/src/com/appstore/yorknodelays/server/Airport.java
@@ -4,16 +4,8 @@ import java.util.List;
 
 public class Airport {
 	
-//	private List<Gate> gate;
-//	private List<Airline> airline;
-	
-	private String words;
-	private int number;
-	
-	public Airport(String words, int number) {
-		this.words = words;
-		this.number = number;
-	}
+	private List<Gate> gate;
+	private List<Airline> airline;
 	
 
 }
