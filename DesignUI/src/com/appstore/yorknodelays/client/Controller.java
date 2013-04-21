debe2e737394193049e9188fa6890b0508c58a39
Elli <nbr500@york.ac.uk>
2013-04-21 02:00:02 +0100

    - More testing

--- DesignUI/src/com/appstore/yorknodelays/client/Controller.java
+++ DesignUI/src/com/appstore/yorknodelays/client/Controller.java
@@ -10,11 +10,16 @@ package com.appstore.yorknodelays.client;
  
 import java.util.ArrayList;
 import java.util.List;
- 
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.editor.client.SimpleBeanEditorDriver;
 import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+/*import com.sencha.gxt.data.shared.ModelKeyProvider;
+import com.sencha.gxt.examples.resources.client.model.Kid;
+import com.sencha.gxt.examples.resources.client.model.Person; */
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -26,23 +31,85 @@ import com.sencha.gxt.widget.core.client.FramedPanel;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
+import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer.HorizontalLayoutData;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
+import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
+import com.sencha.gxt.widget.core.client.grid.ColumnModel;
+import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.grid.editing.GridInlineEditing;
  
+    
 public class Controller implements EntryPoint, IsWidget {
- private Label Label;
-
+	
+	private Label Label;
+	 /* TextField name = new TextField();
+	  TextField company = new TextField();
+	  TextField location = new TextField();
+	   
+	  NumberField<Double> income = new NumberField<Double>(new DoublePropertyEditor()); */
+	   
+	  ListStore<Kid> kidStore = new ListStore<Kid>(props.key());
+	  ListStoreEditor<Kid> kids = new ListStoreEditor<Kid>(kidStore); 
+	   
+	   
+	  @Override
+	  public Widget asWidget() {
+	    FlowPanel container = new FlowPanel();
+	     
+	     
+	    // should be layout based
+	   /* int w = 275;
+	    name.setWidth(w);
+	    company.setWidth(w);
+	    location.setWidth(w);
+	    income.setWidth(w);
+	     
+	    container.add(new FieldLabel(name, "Name"));
+	    container.add(new FieldLabel(company, "Company"));
+	    container.add(new FieldLabel(location, "Location"));
+	     
+	    container.add(new FieldLabel(income, "Income")); */
+	     
+	    List<ColumnConfig<Kid, ?>> columns = new ArrayList<ColumnConfig<Kid,?>>();
+	    ColumnConfig<Kid, String> name = new ColumnConfig<Kid, String>(props.name(), 200, "Fligt Name");
+	    columns.add(name);
+	    ColumnConfig<Kid, String> age = new ColumnConfig<Kid, String>(props.age(), 100, "Origin");
+	    columns.add(age);
+	     
+	     
+	    Grid<Kid> grid = new Grid<Kid>(kidStore, new ColumnModel<Kid>(columns));
+	    grid.setBorders(true);
+	     
+	    grid.getView().setForceFit(true);
+	    GridInlineEditing<Kid> inlineEditor = new GridInlineEditing<Kid>(grid);
+	    inlineEditor.addEditor(name, new TextField());
+	    inlineEditor.addEditor(age, new NumberField<Integer>(new IntegerPropertyEditor()));
+	 
+	    grid.setWidth(382);
+	    grid.setHeight(200);
+	     
+	    FieldLabel kidsContainer = new FieldLabel();
+	    kidsContainer.setText("Kids");
+	    kidsContainer.setLabelAlign(LabelAlign.TOP);
+	    kidsContainer.setWidget(grid);
+	    container.add(kidsContainer);
+	  return container;
+	  }
 // interface Driver extends SimpleBeanEditorDriver<Person, PersonEditor> {}
    
  // private Driver driver = GWT.create(Driver.class);
    
   @Override
+ 
   public Widget asWidget() {
     FramedPanel panel = new FramedPanel();
     panel.setHeadingText("Real Time Flight Information");
     panel.setBodyBorder(false);
     panel.setWidth(400);
     panel.addStyleName("margin-10");
+    
     /*TextArea description = new TextArea();
     description.setAllowBlank(false);
     description.addValidator(new MinLengthValidator(10));
@@ -60,27 +127,25 @@ public class Controller implements EntryPoint, IsWidget {
     }));
      
     Person person = new Person("Darrell Meyer", "Sencha Inc", "GXT", "Washington, DC", 9.99);*/
- 
-  /*  List<Kid> kids = new ArrayList<Kid>();
-    kids.add(new Kid("Alec", 4, new DateWrapper(2004, 1, 1).asDate()));
-    kids.add(new Kid("Lia", 2, new DateWrapper(2005, 1, 1).asDate()));
-    kids.add(new Kid("Andrew", 1, new DateWrapper(2007, 1, 1).asDate()));
-    person.setKids(kids);
-     
-    driver.edit(person);*/
+  HorizontalLayoutContainer p2 = new HorizontalLayoutContainer ();
+  
+    panel.add (p2);
+    
     VerticalLayoutContainer p = new VerticalLayoutContainer();
     panel.add(p);
-    Label spinLabel = new Label("Collision Warming:");
+    Label spinLabel = new Label("Collision Warning:");
     Label = new Label ("nil");
 	 
     p.add(spinLabel, new VerticalLayoutData(1, -1));
  
     return panel;
+   
   }
  
   @Override
-  public void onModuleLoad() {
+  public void onModuleLoad() { 
     RootPanel.get().add(this);
   }
+ }
  
 }
